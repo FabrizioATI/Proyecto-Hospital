e9b8b035682f1e6ebab9ff1690776f99ec08ece8
Nikols Venegas <nikol.venegas@tecsup.edu.pe>
2025-10-01 23:51:37 -0500

    Commit a las urls.

--- app/urls.py
+++ app/urls.py
@@ -6,4 +6,7 @@ urlpatterns = [
     path("medicos/lista/", views.lista_medicos, name="lista_medicos"),
     path('medicos/editar/<int:pk>/', views.editar_medico, name='editar_medico'),
     path('medicos/eliminar/<int:pk>/', views.eliminar_medico, name='eliminar_medico'),
+    path('login', views.login, name='login'),
+    path('', views.login, name='login'),
+    path('register', views.register, name='register'),
 ]
